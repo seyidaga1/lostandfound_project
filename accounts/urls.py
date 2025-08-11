def3ca157e05a10e7b8bec14295d2742892c97cd
seyidaga1 <seyidaga221@gmail.com>
2025-08-11 06:13:38 +0400

    added login and register system

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ from .views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),  # login
+    path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
     path('me/', ProfileView.as_view(), name='user-profile'),
 ]
